9d4e8ad8dbcc43e9cfe3a7cfc132bb31b08d2c15
Max <innovate.center3@gmail.com>
2016-09-21 13:41:40 +0300

    pom fixes

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Max on 05.09.2016.
- */
-public class Main {
-    public static void main(String[] args) {
-
-}
-}
--- src/main/java/services/IUserAuthenticationService.java
+++ src/main/java/services/IUserAuthenticationService.java
@@ -13,12 +13,12 @@ public interface IUserAuthenticationService
      * Validates the password specified by the user
      */
 
-    public void validatePassword(String usid, String password) throws Exception;
+    void validatePassword(String usid, String password) throws Exception;
 
     /**
      * Returns the list of groups whom the user belongs to
      */
 
-    public List<String> getGroups(String usid);
+    List<String> getGroups(String usid);
 
 }
